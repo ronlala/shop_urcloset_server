4656784076a80abbddfb9e44c19c70ed72d8f618
ronlala <61702804+ronlala@users.noreply.github.com>
2025-06-25 12:56:56 -0500

    Update authRouter.js

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -2,7 +2,7 @@ const express = require ("express");
 const passport = require ("passport");
 const router = express.Router();
 
-const {register, login , logout, localLogin} = require("../controllers/authController");
+const {register, login , logout, localLogin} = require("../controllers/authcontroller");
 
 router.post("/register", register);
 router.get("/login", login);
